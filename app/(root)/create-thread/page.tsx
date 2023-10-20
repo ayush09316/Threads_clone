6e5210eaaf71814bcde827cf8ee7c10e7ae7e375
Ayush Sharma <ayushsharma170199@gmail.com>
2023-10-20 11:40:36 +0530

    update in PostThread

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -11,12 +11,12 @@ async function Page() {
   // fetch organization list created by user
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
-
+  
   return (
     <>
       <h1 className='head-text'>Create Thread</h1>
 
-      <PostThread userId={userInfo._id} />
+      <PostThread userId={userInfo._id.toJSON()} />
     </>
   );
 }
